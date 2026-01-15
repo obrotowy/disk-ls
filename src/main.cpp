dfc195d775dd1462725aa588e96f6bdc88e1282b
Emilian Zawrotny <emilian@zawrotny.pl>
2026-01-15 16:20:57 +0100

    Parse FAT only if partition type is FAT

--- src/main.cpp
+++ src/main.cpp
@@ -15,20 +15,22 @@ int main(int argc, const char** argv) {
     printf("\tType: 0x%x\n", part.type);
     printf("\tSize: %d sectors\n", part.size);
     printf("\tBegin LBA: 0x%x\n", part.begin_lba);
-    FAT32 fs(part.read_sector(0));
-    printf("FAT32 Information:\n");
-    printf("\tVolume Label: %s\n", fs.Label);
-    printf("\tCluster size: %d sectors\n", fs.SectorsPerCluster);
-    printf("\tReserved Sectors: %d\n", fs.ReservedSectors);
-    printf("\tFAT copies: %d\n", fs.FatCopies);
-    printf("\tFAT Size: %d sectors\n", fs.SectorsPerFAT);
-    const uint32_t fs_data_offset = fs.ReservedSectors + fs.FatCopies * fs.SectorsPerFAT;
-    const uint8_t* root_dir_sector = part.read_sector(fs_data_offset);
-    BasicFileRecord file = fs.readFileRecord(root_dir_sector, 1);
-    printf("Short file name: %s\n", file.filename);
-    printf("Position in FAT: %d\n", file.position_in_fat);
-    printf("File size: %d B\n", file.size);
-    const uint8_t* file_data = part.read_sector(fs_data_offset + (file.position_in_fat - 2) * fs.SectorsPerCluster);
-    printf("File data: %s\n", file_data);
+    if (part.type == FAT32_LBA) {
+      FAT32 fs(part.read_sector(0));
+      printf("FAT32 Information:\n");
+      printf("\tVolume Label: %s\n", fs.Label);
+      printf("\tCluster size: %d sectors\n", fs.SectorsPerCluster);
+      printf("\tReserved Sectors: %d\n", fs.ReservedSectors);
+      printf("\tFAT copies: %d\n", fs.FatCopies);
+      printf("\tFAT Size: %d sectors\n", fs.SectorsPerFAT);
+      const uint32_t fs_data_offset = fs.ReservedSectors + fs.FatCopies * fs.SectorsPerFAT;
+      const uint8_t* root_dir_sector = part.read_sector(fs_data_offset);
+      BasicFileRecord file = fs.readFileRecord(root_dir_sector, 1);
+      printf("Short file name: %s\n", file.filename);
+      printf("Position in FAT: %d\n", file.position_in_fat);
+      printf("File size: %d B\n", file.size);
+      const uint8_t* file_data = part.read_sector(fs_data_offset + (file.position_in_fat - 2) * fs.SectorsPerCluster);
+      printf("File data: %s\n", file_data);
+    }
   }
 }
